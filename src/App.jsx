60b31a1a341592ec7f32d8428a5f125ab2938b93
wilberth-cl <wilberthfrancisco.cl@gmail.com>
2023-02-07 13:03:37 -0600

    simple example api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import MyRoutes from './component/MyRoutes'
+import MyRoutes from './component/MyRoutes';
+
+import axios from 'axios';
+const baseURL = 'https://jsonplaceholder.typicode.com/posts/1';
 
 function App() {
+
+  const [post, setPost] = React.useState(null);
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
 
   return (
     <BrowserRouter>
@@ -10,6 +22,12 @@ function App() {
 
         <MyRoutes />
        
+      </div>
+      <div>
+        <h1>{post.userId}</h1>
+        <p>{post.id}</p>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
       </div>
     </BrowserRouter>
   );
